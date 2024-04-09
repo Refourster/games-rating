aecd61fa804325dcaa0fd3640af59604b9c73bc2
Makar <147124109+Refourster@users.noreply.github.com>
2024-04-09 20:17:16 +0500

    Add files via upload

---
+++ appModules/rating/index.js
@@ -0,0 +1,12 @@
+const config = require("./config");
+const makeRatingFile = require("./rating-file");
+const createRating = require("./calculations");
+const updateRating = require("./calculations");
+
+
+module.exports = {
+    config,
+    makeRatingFile,
+    createRating,
+    updateRating
+}
